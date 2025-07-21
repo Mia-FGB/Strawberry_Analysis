d27e2d7d059c2cbabe934bed62435727b5dae77b
Mia-FGB <92913584+Mia-FGB@users.noreply.github.com>
2025-07-21 12:36:23 +0100

    updating the heatmap script and the phibase analysis script to keep them separate

--- Heatmaps.R
+++ Heatmaps.R
@@ -1,3 +1,5 @@
+# Script to plot heatmaps of the PHIbase alignment data from Tiptree
+
 library(ggplot2)
 library(RColorBrewer)
 library(tidyr)
@@ -21,7 +23,8 @@ raw_dec_data <- read.csv("Tiptree_taxa_counts_initial_analysis/Tiptree_Dec_unfil
 
 # Process the data ---
 #Want to join the two datasets
-all_heatmap_data <- rbind(filter_f_a , raw_dec_data)
+all_heatmap_data <- rbind(filter_f_a , raw_dec_data) %>% 
+  rename(Species = species) 
 
 # Process the date column
 all_heatmap_data$Date.collected = as.Date(all_heatmap_data$Date.collected, format="%d/%m/%Y")
@@ -37,71 +40,6 @@ all_heatmap_filtered <-
     log_hits = log10(hits_per_100.000)) %>%     #log scale-Mutate the data to add a new row
   mutate(Sample = as.character(Sample))
 
-
-
-# Plotting ---
-# Heatmap function:
-plot_species_heatmap <- function(data,
-                                 x = "Sample",
-                                 y = "species",
-                                 xlab = x,
-                                 fill = "log_hits",
-                                 title = "Species Abundance Heatmap",
-                                 save_path = NULL,
-                                 width = 12,
-                                 height = 18) {
-  
-  # Compute abundance
-  species_abundance <- data %>%
-    group_by(species) %>%
-    summarise(total_abundance = sum(log_hits, na.rm = TRUE)) %>%
-    arrange(desc(total_abundance))
-  
-  # Apply the order to the main data (most abundant species at the bottom)
-  data <- data %>%
-    mutate(species = factor(species, levels = species_abundance$species))
-  
-  # Dynamically map aesthetics
-  aes_mapping <- aes_string(x = x, y = y, fill = fill)
-  
-  # Create plot
-  p <- ggplot(data, mapping = aes_mapping) +
-    geom_tile() +
-    
-    # scale_fill_distiller(
-    #   name = "Hits per 100,000",
-    #   palette = "YlOrRd",
-    #   labels = function(x) scales::comma(10^x)
-    #   ) +
-    
-    scale_fill_viridis_c(
-      name = "Hits per 100,000",
-      labels = function(x) round(10^x),
-      option = "B",  # Options: "D", "C", "B", etc.
-      direction = 1
-    ) +
-  
-    theme_bw() +
-    labs(title = title, x = xlab, y = y) +
-    theme(
-      axis.text.x = element_text(size = 12),
-      axis.text.y = element_text(size = 12),
-      axis.title = element_text(size = 16),
-      plot.title = element_text(size = 18, hjust = 0.5),
-      legend.title = element_text(size = 14),
-      legend.text = element_text(size = 12),
-      legend.key.size = unit(1.5, "cm"),
-      legend.key.width = unit(0.4, "cm")
-    )
-  
-  # Save plot if path provided
-  if (!is.null(save_path)) {
-    ggsave(filename = save_path, plot = p, width = width, height = height)
-  }
-  
-  return(p)
-}
-
 #Preparing filtered data ----
 
 #Adding a col for month
@@ -111,18 +49,19 @@ all_heatmap_filtered$month <-
 #filtering out taxa which are only present in <10 samples - less stringent when grouped
 all_heatmap_filtered_10taxa <-
   all_heatmap_filtered %>% 
-  group_by(species) %>% 
+  group_by(Species) %>% 
   filter(n()>=10)
 
 # filtering out taxa which are only present in <15 samples - for all the samples to be plotted seperate
 all_heatmap_filtered_15taxa <- all_heatmap_filtered %>% 
-  group_by(species) %>% 
+  group_by(Species) %>% 
   filter(n()>=15)
 
 #reorder month cols
 all_heatmap_filtered_10taxa$month <- 
   factor(all_heatmap_filtered_10taxa$month,
-         levels=c("Dec", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"))
+         # levels=c("Dec", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"))
+         levels=c("Aug", "Jul", "Jun", "May", "Apr", "Mar", "Feb", "Dec"))
 
 # Change labels and order
 all_heatmap_filtered_10taxa <- all_heatmap_filtered_10taxa %>%
@@ -130,33 +69,139 @@ all_heatmap_filtered_10taxa <- all_heatmap_filtered_10taxa %>%
                            levels = c("F", "RG", "LG"), 
                            labels = c("Field", "Greenhouse 1", "Greenhouse 2")))
 
+
+
+# Plotting ---
+# Heatmap function:
+plot_species_heatmap <- function(data,
+                                 x = "Sample",
+                                 y = "Species",
+                                 xlab = x,
+                                 fill = "log_hits",
+                                 title = "Species Abundance Heatmap",
+                                 save_path = NULL,
+                                 width = 12,
+                                 height = 18,
+                                 theme_extra = NULL,
+                                 flip = FALSE) {
+  
+  # Compute abundance
+  species_abundance <- data %>%
+    group_by(Species) %>%
+    summarise(total_abundance = sum(log_hits, na.rm = TRUE)) %>%
+    arrange(desc(total_abundance))
+  
+  # Apply the order to the main data (most abundant species at the bottom)
+  data <- data %>%
+    mutate(Species = factor(Species, levels = species_abundance$Species))
+  
+  # Swap x/y if flip = TRUE
+  if (flip) {
+    aes_mapping <- aes_string(x = y, y = x, fill = fill)
+  } else {
+    aes_mapping <- aes_string(x = x, y = y, fill = fill)
+  }
+  
+  # Create plot
+  p <- ggplot(data, mapping = aes_mapping) +
+    geom_tile() +
+    
+    scale_fill_viridis_c(
+      name = "Hits per 100,000",
+      labels = function(x) round(10^x),
+      option = "C",  # Options: "D", "C", "B", etc.
+      direction = 1
+    ) +
+  
+    theme_bw() +
+    labs(
+      title = title,
+      x = ifelse(flip, y, xlab),
+      y = ifelse(flip, xlab, y)
+    ) +
+    theme(
+      axis.text.x = element_text(size = 12),
+      axis.text.y = element_text(size = 12),
+      axis.title = element_text(size = 16),
+      plot.title = element_text(size = 18, hjust = 0.5),
+      legend.title = element_text(size = 14),
+      legend.text = element_text(size = 12),
+      legend.key.size = unit(1.5, "cm"),
+      legend.key.width = unit(0.4, "cm")
+    ) +
+    theme_extra
+  
+  # Save plot if path provided
+  if (!is.null(save_path)) {
+    ggsave(filename = save_path, plot = p, width = width, height = height)
+  }
+  
+  return(p)
+}
+
+
 # Call the function ---
 # All samples-
 plot_species_heatmap(
   data = all_heatmap_filtered_15taxa,
   x = "Sample",
-  y = "species",
+  y = "Species",
   title = "",
   save_path = "Graphs/Heatmaps/heatmap_by_sample.pdf",
   width = 18
 )
 
-# Group by month -
+
+# Group by month - Vertical plot
 plot_species_heatmap(
   data = all_heatmap_filtered_10taxa,
   x = "month",
-  y = "species",
+  y = "Species",
   xlab="Month",
   title = "",
-  save_path = "Graphs/Heatmaps/heatmap_by_month.pdf"
+  save_path = "Graphs/Heatmaps/heatmap_by_month.pdf",
+  height = 18,
+  width = 8
 )
 
-# Plot by location -
+# Group by month - Horizonta plot
+plot_species_heatmap(
+  data = all_heatmap_filtered_10taxa,
+  x = "month",
+  y = "Species",
+  xlab="Month",
+  title = "",
+  save_path = "Graphs/Heatmaps/heatmap_by_month_rot.pdf",
+  flip = TRUE,
+  height = 6,
+  width = 18,
+  theme_extra = theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+)
+
+# Plot by location - vertical 
 plot_species_heatmap(
   data = all_heatmap_filtered_10taxa,
   x = "location",
-  y = "species",
+  y = "Species",
   xlab = "Location",
   title = "",
-  save_path = "Graphs/Heatmaps/heatmap_by_location.pdf"
+  save_path = "Graphs/Heatmaps/heatmap_by_location.pdf",
+  height = 18,
+  width = 12
 )
+
+# Plot by location - horizontal  
+plot_species_heatmap(
+  data = all_heatmap_filtered_10taxa,
+  x = "location",
+  y = "Species",
+  xlab = "Location",
+  title = "",
+  save_path = "Graphs/Heatmaps/heatmap_by_location_rot.pdf",
+  flip = TRUE,
+  height = 5,
+  width = 18,
+  theme_extra = theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+)
+
+ 
